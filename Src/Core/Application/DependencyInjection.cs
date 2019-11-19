ea5a5ecf46618b2250addc0f1cd3dd95658160f2
Wei Li <jimleeii@gmail.com>
2019-11-18 22:03:50 -0700

    Add comments

--- Src/Core/Application/DependencyInjection.cs
+++ Src/Core/Application/DependencyInjection.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Neptune.Core.Application
 {
+    /// <summary>
+    /// Extension for service collection to inject current class lib required dependencies
+    /// </summary>
     public static class DependencyInjection
     {
+        /// <summary>
+        /// Performs denpendency injection at Startup.cs
+        /// </summary>
+        /// <param name="services">Service collection</param>
+        /// <returns>Service collection</returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Inject dependencies
+
+            return services;
+        }
     }
 }
